bd71deabdfe7e3513aaf4f5320fc9cdf805ba547
ichirxx <kyrowxx@gmail.com>
2026-05-12 18:46:02 +0800

    Update storage.py

--- storage.py
+++ storage.py
@@ -2,7 +2,7 @@ import os
 import psycopg2
 from psycopg2 import sql
 
-_DB_URL = "postgresql://neondb_owner:npg_qOkcVZ19Utdx@ep-delicate-breeze-ap808hxk-pooler.c-7.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
+_DB_URL = "postgresql://neondb_owner:npg_q0kcVZ19Utdx@ep-delicate-breeze-ap808hxk-pooler.c-7.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
 
 def _get_conn():
     return psycopg2.connect(_DB_URL)
@@ -51,6 +51,4 @@ def load(table, key, default=None):
     conn.close()
     if row:
         return eval(row[0])
-    return de
-    fault 
-
+    return default
