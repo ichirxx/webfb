35c18c584dec9eda5fb23c3ab9e84a86bebf847f
ichirxx <kyrowxx@gmail.com>
2026-05-12 19:32:53 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1117,7 +1117,7 @@ def webhook_email():
     })
 
 
-if __name__ == '__main__':
-    auth.start_bot()
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=False, threaded=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port, debug=False)
+
